079013a1f4af96f9ab8f894aeac041eb6347da95
simranmaurya123 <simran.maurya.932@gmail.com>
2024-07-09 12:38:40 +0530

    two neww files

---
+++ DataSizeRange.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class DataSizeRange
+{
+
+    public static void main(String arg[])
+{
+ 	
+	System.out.println("Float Min: " + Float.MIN_VALUE);
+	System.out.println("Float Max: " + Float.MAX_VALUE);
+	System.out.println("Float Bytes: " + Float.BYTES);
+
+
+
+	}
+
+}
