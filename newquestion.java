c556438f6d1ee7990ce8e9c73befba566def9591
simranmaurya123 <simran.maurya.932@gmail.com>
2024-07-29 13:22:08 +0530

    full code

--- newquestion.java
+++ newquestion.java
@@ -1,8 +1,11 @@
  public class newquestion{
     public static void main(String[] args) {
+       
+       System.out.println("To check whether the given number is binary or not");
+       
         int b=10110001;
         String s=String.valueOf(b);
-
+     
         System.out.println(s.matches("[01]*"));
 
 
@@ -13,6 +16,27 @@
         
         System.out.println("first : "+a.matches("[0-9][a-f][A-F]*"));
         System.out.println("second : "+c.matches("[0-9][a-f][A-F]"));
+        
+
+        String str1="02/08/2006";
+
+        System.out.println("date:"+ str1.matches("[0-3][0-9]/[01][0-9]/[0-9]{4}"));
+
+
+        String str2="a!B%c#D*8%";
+        String s1=str2.replaceAll("[!-*]","");
+
+        System.out.println(s1);
+
+
+        String str3="  abc  df  hj   ij    ikl  mns    ";
+        
+        String s2= str3.replaceAll("\\s+"," ").trim().toUpperCase();
+        String words[]=s2.split("\\s");
+        System.out.println("length : "+words.length);
+
+        System.out.println(s2);
+
 
 
         
